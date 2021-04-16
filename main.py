67fd266703da5b779706520ea958e98503e66462
Marti March <mmll1199@protonmail.com>
2021-04-16 03:28:58 +0200

    Se añaden los umbrles que permiten detectar movimiento. Cada 15 minutos se renueva la cantidad de esquinas tomadas como referencia.

--- main.py
+++ main.py
@@ -9,19 +9,25 @@
 """
 import numpy as np
 import cv2
+from datetime import datetime
 
 camara = cv2.VideoCapture(1, cv2.CAP_DSHOW)
+umbralSuperior = 1.10
+umbralInferior = 0.90
+now = 0
+tiempoActual = 0
+nEsquinas = 0
 
 while True:
     _, imagen = camara.read()
 
-    # La imágnen a color se trnsforma en una imágen en gris
+    # La imagnen a color se transforma en una imagen en gris
     imagenGris = cv2.cvtColor(imagen, cv2.COLOR_BGR2GRAY)
 
     """
      Se obtienen las N mejor imágenes.
      
-      1 - Imágen sobre la que buscar las esquinas. 
+      1 - Imagen sobre la que buscar las esquinas. 
     
       2 - Cantidad de esquinas deseadas. Son las mejores esquinas.
       
@@ -30,18 +36,30 @@ while True:
           calidad inferior a 15 se rechazarán (1500 * 0.01). 
           
       4 - Distancia mímina que ha de existrir entre las esquinas. Es lo que nos permite crear una distribución uniforme de
-          las esquinas.    
+          las esquinas a lo largo de la imágen.    
     """
-    esquinas = cv2.goodFeaturesToTrack(imagenGris, 25, 0.01, 10)
+    esquinas = cv2.goodFeaturesToTrack(imagenGris, cv2.countNonZero(imagenGris), 0.01, 10)
 
     # Introducimos las esquinas en un vector de tipo int64
     esquinas = np.int0(esquinas)
 
-    # Se dibujan círculos donde se situen las esquinas
+    # Se dibujan círculos allá donde se encuentre una esquina y se cuenta cuantas esquinas existen.
+    nEsquinas = 0
     for i in esquinas:
         x,y = i.ravel()
         cv2.circle(imagen, (x,y), 3, 255, -1)
+        nEsquinas = nEsquinas + 1
 
-    cv2.imshow("Harris - color", imagen);
+    # Cada 15 minutos se renuevan los puntos de referencia usados para comparar
+    now = datetime.now()
+    if now.timestamp() - tiempoActual > 9000000 or tiempoActual == 0:
+        tiempoActual = now.timestamp()
+        puntosRelevantes = nEsquinas
+
+    # Para saber si se ha producido movimiento lo que se hace es comparar si la cantidad de puntos entra dentro de los umbrales.
+    if nEsquinas > (puntosRelevantes * umbralSuperior) or nEsquinas < (puntosRelevantes * umbralInferior):
+        print("MOVIMIENTO")
+
+    cv2.imshow("Shi-Tomasi - color", imagen);
     if cv2.waitKey(2) & 0xFF == ord('q'):
         break
